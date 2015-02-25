b7849f8a6a9c9bc58c6fde17741b44215fadeddf
Matthieu Bruneaux <matthieu.bruneaux@gmail.com>
2015-02-25 15:25:31 +0200

    Fix publishconf.py with site url and relative urls

--- pelican_website/publishconf.py
+++ pelican_website/publishconf.py
@@ -10,8 +10,8 @@ import sys
 sys.path.append(os.curdir)
 from pelicanconf import *
 
-SITEURL = ''
-RELATIVE_URLS = False
+SITEURL = 'http://mdjbru-teaching-material.github.io/roscoff_course_2015'
+RELATIVE_URLS = True
 
 #FEED_ALL_ATOM = 'feeds/all.atom.xml'
 #CATEGORY_FEED_ATOM = 'feeds/%s.atom.xml'
