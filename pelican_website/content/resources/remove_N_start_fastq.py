85fd4d6c4b74dad1f9e202d0e8ae5a806ab90a77
Matthieu Bruneaux <matthieu.bruneaux@gmail.com>
2015-03-09 01:43:34 +0200

    Add barcode and restriction site removal

---
+++ pelican_website/content/resources/remove_N_start_fastq.py
@@ -0,0 +1,35 @@
+### * Description
+
+# Script to remove the N first nucleotide positions in a FASTQ file.
+#
+# The script assumes that there are no blank lines in the input files,
+# and that each record has four lines. It will remove N characters
+# from the second and fourth lines of each record.
+#
+# Usage:
+# python script.py inputFile1 [inputFile2 ...] N_positions
+
+### * Setup
+
+### ** Import
+
+import sys
+
+### ** Parameters
+
+N_ARGS = len(sys.argv) - 1
+print(N_ARGS)
+INPUT_FILES = sys.argv[1: N_ARGS]
+N_TO_REMOVE = int(sys.argv[N_ARGS])
+
+### * Run
+
+for INPUT_FILE in INPUT_FILES :
+    print("Removing %i nucleotides in: %s" % (N_TO_REMOVE, INPUT_FILE))
+    with open(INPUT_FILE, "r") as fi :
+        with open(INPUT_FILE + ".clean", "w") as fo :
+            for l in fi :
+                fo.write(l)
+                fo.write(fi.next()[N_TO_REMOVE:])
+                fo.write(fi.next())
+                fo.write(fi.next()[N_TO_REMOVE:])
