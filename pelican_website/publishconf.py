585b22f88d85af288593523cf897c31f00ddd607
Matthieu Bruneaux <matthieu.bruneaux@gmail.com>
2015-03-09 01:10:57 +0200

    Demultiplexing complete

--- pelican_website/publishconf.py
+++ pelican_website/publishconf.py
@@ -17,7 +17,7 @@ DELETE_OUTPUT_DIRECTORY = True
 
 # Org reader
 PLUGIN_PATHS = ["."]
-PLUGINS = ["render_math"]
+PLUGINS = ["subcategory"]
 #ORG_READER_EMACS_LOCATION = "/usr/bin/emacs"
 #PLUGIN_PATHS = ["./extract_toc"]
 #PLUGINS = ["extract_toc"]
